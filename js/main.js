13c54dec9831a8b07bc77110e02a6d4b5e9854e8
Roman Dovgii <romandovgiiwork@gmail.com>
2020-02-03 14:30:55 +0300

    almost final

--- js/main.js
+++ js/main.js
@@ -8,16 +8,20 @@ var LIKES_MAX_NUMBER = 200;
 var COMMENTS_MIN_NUMBER = 1;
 var COMMENTS_MAX_NUMBER = 15;
 
-// counters for elements//
+var PHOTOS_COUNTER = 25;
 
-var photoDescriptionCounter = 25;
+var IMAGE_TEMPLATE = document.querySelector('#picture').content.querySelector('.picture');
+var PICTURES_CONTAINER = document.querySelector('.pictures');
+
+// variables//
+
 var avatarDescriptionCounter = 6;
 var pathToPhotos = 'photos/';
 var pathToImg = 'img/avatar-';
 
 // functions//
 
-// function #1//
+// function #1 - generates ta url array//
 var urlArrayCreator = function (type, arrayLength) {
   var array = [];
   var typeExtension = '';
@@ -36,40 +40,93 @@ var urlArrayCreator = function (type, arrayLength) {
   return array;
 };
 
-// function #2//
+// function #2 - generates a random number//
 var randomNumberGenerator = function (min, max) {
   var number;
   number = Math.floor(Math.random() * (max - min) + min);
   return number;
 };
 
-// function #3//
-var commentObjectGeneration = function () {
-  var comment = {
-    avatar: avatarsUrlArray[randomNumberGenerator(0, avatarsUrlArray.length)],
-    message: messagePartsArray[randomNumberGenerator(0, messagePartsArray.length)],
-    name: namesArray[randomNumberGenerator(0, namesArray.length)]
-  };
+// function #3 - generates a random comment//
+
+var randomCommentGenerator = function (pieces) {
+  var comment = '';
+  var previousPart = '';
+  var piecesArray = Array.from(messagePartsArray);
+  for (var i = 0; i < pieces; i++) {
+    previousPart = piecesArray[randomNumberGenerator(0, piecesArray.length)];
+    comment = comment + previousPart + ' ';
+    piecesArray.splice(piecesArray.indexOf(previousPart), 1);
+  }
   return comment;
 };
 
+// function #4 - generates a comment object//
+var commentObjectGeneration = function (count) {
+  var commentsList = [];
+  for (var i = 0; i < count; i++) {
+    var comment = {
+      avatar: avatarsUrlArray[randomNumberGenerator(0, avatarsUrlArray.length)],
+      message: randomCommentGenerator(2),
+      name: namesArray[randomNumberGenerator(0, namesArray.length)]
+    };
+    commentsList.push(comment);
+  }
+  return commentsList;
+};
 
-// function #4//
-var pictureObjectGeneration = function () {
+
+// function #5 - generates a picture object//
+var pictureObjectGeneration = function (photosArray) {
   var picture = {
-
+    url: photosArray[randomNumberGenerator(0, photosArray.length)],
+    description: '',
+    likes: randomNumberGenerator(LIKES_MIN_NUMBER, LIKES_MAX_NUMBER),
+    comments: commentObjectGeneration(randomNumberGenerator(COMMENTS_MIN_NUMBER, COMMENTS_MAX_NUMBER))
   };
   return picture;
 };
 
+// function #6 - generates images objects array//
+
+var photoArrayGeneration = function () {
+  var localPhotoObjectArray = [];
+  var localPhotosUrlArray = Array.from(photosUrlArray);
+  for (var i = 0; i < PHOTOS_COUNTER; i++) {
+    localPhotoObjectArray.push(pictureObjectGeneration(localPhotosUrlArray));
+    localPhotosUrlArray.splice(localPhotosUrlArray.indexOf(localPhotoObjectArray[i].url), 1);
+  }
+  return localPhotoObjectArray;
+};
+
+// function #7 - renders images blocks//
+
+var renderImage = function (number, photoObjectArray) {
+  var photoBlock = IMAGE_TEMPLATE.cloneNode(true);
+  photoBlock.querySelector('.picture__img').src = photoObjectArray[number].url;
+  photoBlock.querySelector('.picture__comments');
+  photoBlock.querySelector('.picture__likes').textContent = photoObjectArray[number].likes;
+  var fragment = document.createDocumentFragment();
+  fragment.appendChild(photoBlock);
+  return fragment;
+};
+
+// #function #8 -appends rendered images blocks//
+
+var appendImage = function () {
+  var photoObjectArray = photoArrayGeneration();
+  for (var i = 0; i < PHOTOS_COUNTER; i++) {
+    PICTURES_CONTAINER.appendChild(renderImage(i, photoObjectArray));
+  }
+};
 // arrays//
 
-var photosUrlArray = urlArrayCreator(pathToPhotos, photoDescriptionCounter);
+var photosUrlArray = urlArrayCreator(pathToPhotos, PHOTOS_COUNTER);
 var avatarsUrlArray = urlArrayCreator(pathToImg, avatarDescriptionCounter);
 var namesArray = ['Аарон', 'Авраам', 'Агафон', 'Азат', 'Айрат', 'Артём', 'Арам', 'Борис', 'Блез', 'Богдан', 'Булат', 'Адольф', 'Бронислав', 'Бестрева', 'Будёна', 'Валентин', 'Вит', 'Василий', 'Влад', 'Владимир', 'Вильгельм', 'Евгений', 'Евдоким', 'Евсей', 'Жерар', 'Ждан', 'Александра', 'Закир', 'Зиновий', 'Дазвсемир', 'Даздранагон', 'Даздраперма', 'Далис', 'Кукуцаполь', 'Вилюр', 'Лоренс', 'Леон', 'Лев', 'Ленар', 'Любомир', 'Панкрат', 'Павел', 'Пётр', 'Равиль', 'Радик', 'Рамзан', 'Ролан', 'Рудольф', 'Радий', 'Полоний', 'Саша', 'Тигран', 'Томас', 'Серафим', 'Тайлер', 'Фарид', 'Савва', 'Фазиль', 'Хабиб', 'Самвел', 'Сани', 'Сергей', 'Спартак', 'Султан', 'Сидор', 'Станислав', 'Ладимир', 'Влада', 'Тарас', 'Тихон', 'Фёдор', 'Флор', 'Филипп', 'Фарид', 'Фазиль', 'Цезарь', 'Цефас', 'Хаким', 'Чарльз', 'Артур', 'Чеслав', 'Шарль', 'Эдгар', 'Эдуард', 'Эрик', 'Эльдар', 'Ильдар', 'Юрий', 'Юнус', 'Юлиан', 'Роман', 'Юстиниан', 'Ян', 'Яков', 'Федот', 'Фома', 'Филипп', 'Фридрих', 'Феликс', 'Ганс', 'Фриц', 'Иван', 'Ильшат', 'Искандер', 'Игнатий', 'Клим', 'Ким', 'Корнелий', 'Коре', 'Клаус', 'Казбек', 'Мар', 'Марий', 'Мартин', 'Микула', 'Михаил', 'Милослав', 'Моисей', 'Иисус', 'Никодим', 'Никола', 'Нестор', 'Ник', 'Николай', 'Нильс', 'Нариман', 'Осип', 'Иосиф', 'Олег', 'Огюст', 'Орландо', 'Оскар', 'Остап', 'Орест', 'Рамон', 'Рафик', 'Ринат', 'Ренат', 'Рожден', 'Рэй', 'Рубен', 'Радомир', 'Роланд', 'Ролан', 'Равиль', 'Перри', 'Прохор', 'Патрик', 'Платон', 'Педро', 'Оливер', 'Остин', 'Мэтью', 'Ян', 'Яков', 'Яромир', 'Ярослав', 'Лев', 'Ладо', 'Леонард', 'Лука', 'Максуд', 'Людвиг', 'Лукиллиан', 'Люций', 'Лукьян', 'Леонид', 'Леон', 'Ираклий', 'Иннокентий', 'Исидор', 'Итан', 'Исмаил', 'Исааклий', 'Ильнур', 'Игорь', 'Имран', 'Ильгиз', 'Ильяс', 'Исаак', 'Ислам', 'Захар', 'Зиновий', 'Зураб', 'Зигмунд', 'Зуфар', 'Еремей', 'Евстахий', 'Елисей', 'Ефрем', 'Ефим', 'Егор', 'Исидор', 'Мар', 'Марс', 'Борислав', 'Блез', 'Архип', 'Анфим', 'Анар', 'Алмаз', 'Ангел', 'Августин', 'Аваз', 'Агапит', 'Агап', 'Азиз', 'Бахрам', 'Ава', 'Ангелина', 'Алана', 'Альфия', 'Аэлита', 'Аяна', 'Ася', 'Ария', 'Антонина', 'Анфиса', 'Асель', 'Афина', 'Анна', 'Бажена', 'Ванда', 'Бела', 'Божена', 'Грета', 'Галия', 'Галина', 'Дина', 'Диля', 'Анастасия', 'Джессика', 'Дарья', 'Даша', 'Ева', 'Екатерина', 'Елена', 'Евгения', 'Жанна', 'Жаклин', 'Маша', 'Мария', 'Леся', 'Лада', 'Лиза', 'Лета', 'Мадлен', 'Мика', 'Мила', 'Надежда', 'HELL9000', 'Памела', 'Полина', 'Мия', 'Мио', 'Марта', 'Милица', 'Мариям', 'Оксана', 'Хилари', 'Хлоя', 'Юнона', 'Ясмниа', 'Юнона', 'Эмилия', 'Шерил', 'Юлия', 'Антонина', 'Алиса', 'Айлин', 'Азалия', 'Ада', 'Агафья', 'Асия', 'Амина', 'Анита', 'Вера', 'Валентина', 'Вита', 'Вероника', 'Валерия', 'Василиса', 'Веста', 'Венера', 'Виола', 'Владислава', 'Виолетта', 'Викторина', 'Василина', 'Владана'];
 var messagePartsArray = ['Всё отлично!', 'В целом всё не плохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-// variables//
+
 
 // body//
-var test = commentObjectGeneration();
-console.log(test);
+
+appendImage();
