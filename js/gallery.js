3b3d66c9b12edfe3f9ab4b7dbf0a2f7e2ae797e5
Roman Dovgii <romandovgiiwork@gmail.com>
2020-03-10 16:50:38 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -5,37 +5,31 @@
   var IMAGE_TEMPLATE = document.querySelector('#picture').content.querySelector('.picture');
   var PHOTOS = [];
 
-  function renderImage(number, photoObjects) {
+  function renderImage(photoObjects) {
     var photoBlock = IMAGE_TEMPLATE.cloneNode(true);
     var fragment = document.createDocumentFragment();
 
-    photoBlock.querySelector('.picture__img').src = photoObjects[number].url;
-    photoBlock.querySelector('.picture__comments').textContent = photoObjects[number].comments.length;
-    photoBlock.querySelector('.picture__likes').textContent = photoObjects[number].likes;
+    photoBlock.querySelector('.picture__img').src = photoObjects.url;
+    photoBlock.querySelector('.picture__comments').textContent = photoObjects.comments.length;
+    photoBlock.querySelector('.picture__likes').textContent = photoObjects.likes;
     fragment.appendChild(photoBlock);
 
     return fragment;
   }
 
-  function appendImage(photos) {
+  function createImageList(photos) {
 
     for (var i = 0; i < photos.length; i++) {
-      PICTURES_CONTAINER.appendChild(renderImage(i, photos));
+      PICTURES_CONTAINER.appendChild(renderImage(photos[i]));
     }
 
   }
 
-  function returnArray(photos, callback) {
-    PHOTOS = photos;
-    callback();
-  }
-
   window.backend.load(function (photos) {
-    appendImage(photos);
-    returnArray(photos, function () {
-      window.gallery = {
-        data: PHOTOS
-      };
-    });
+    createImageList(photos);
+    PHOTOS = photos;
+    window.gallery = {
+      data: PHOTOS
+    };
   });
 })();
