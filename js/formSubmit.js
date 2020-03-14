c346a741606162d2eb2bdc08edf40c2a708caa31
Roman Dovgii <romandovgiiwork@gmail.com>
2020-03-14 11:31:39 +0300

    fix

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -1,21 +1,25 @@
 'use strict';
 
 (function () {
+  var SUBMIT = document.querySelector('.img-upload__submit');
   var FORM = document.querySelector('.img-upload__form');
   // var SUBMIT_BUTTON = FORM.querySelector('.img-upload__submit');
 
   function showSuccessPopup() {
+    SUBMIT.disabled = false;
     window.formReset.closeAndReset();
     window.formSuccess.render();
   }
 
   function showErrorPopup() {
+    SUBMIT.disabled = false;
     window.formReset.closeAndReset();
     window.formError.render();
   }
 
   FORM.addEventListener('submit', function (evt) {
     evt.preventDefault();
+    SUBMIT.disabled = true;
 
     window.backend.upload(new FormData(FORM), showSuccessPopup, showErrorPopup);
   });
